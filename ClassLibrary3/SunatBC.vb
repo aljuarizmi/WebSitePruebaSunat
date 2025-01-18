55da766c9f58c644cf4e396415fbf9f421bd1197
aljuarizmi <aljuarizmi@gmail.com>
2025-01-18 17:33:56 -0500

    se pone un comentario de apostrofe

--- ClassLibrary3/SunatBC.vb
+++ ClassLibrary3/SunatBC.vb
@@ -40,7 +40,7 @@ Public Class SunatBC
                     Return objE
                 End If
                 Dim DATA As String
-                'URL = $"http://www.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias?accion=consPorRuc&nroRuc={RUC}&codigo={TextoCache}&tipdoc=1"
+                ''URL = $"http://www.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias?accion=consPorRuc&nroRuc={RUC}&codigo={TextoCache}&tipdoc=1"
                 If tipo = EnumTipoDato.RUC Then
                     'Consulta de RUC
                     URL = $"https://api.apis.net.pe/v1/ruc?numero=" & DATO
